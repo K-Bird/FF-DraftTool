3048c7556bf8560b0062a61ee5518a324b8c5f09
Kenton Lewis <kentonp.tech@gmail.com>
2015-08-06 10:04:54 -0400

    Initial Commit
    Index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Birdman Draft Tool</title>
+        <link href="libs/css/bootstrap.css" rel="stylesheet" type="text/css">
+        <link href="libs/css/bootstrap-theme.css" rel="stylesheet" type="text/css">
+        <link href="libs/css/simple-sidebar.css" rel="stylesheet" type="text/css">
+        <script src="libs/js/jquery.js"></script>
+        <script src="libs/js/bootstrap.js"></script>
+    </head>
+    <body>
+        
+    </body>
+</html>
